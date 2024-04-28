7c1c8563f316da737428f2e5d176c4faec98d008
Anas Ahammad <anasahammad2002@gmail.com>
2024-04-29 01:13:58 +0600

    env added

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBrGCzJ9CEt8BFo4yoiE8V9Igzz0kDo0VI",
-  authDomain: "tropitour-b6096.firebaseapp.com",
-  projectId: "tropitour-b6096",
-  storageBucket: "tropitour-b6096.appspot.com",
-  messagingSenderId: "559906640048",
-  appId: "1:559906640048:web:fae12592ccf6490181ab75"
+  apiKey:import.meta.env.VITE_APIKEY,
+  authDomain:import.meta.env.VITE_AUTHDOMAIN,
+  projectId:import.meta.env.VITE_PROJECTID,
+  storageBucket:import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId:import.meta.env.VITE_MESSAGINGSENDERID,
+  appId:import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase

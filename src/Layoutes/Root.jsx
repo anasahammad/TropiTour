abc416349840cf6a01050954d06f8071ce8dfd73
Anas Ahammad <anasahammad2002@gmail.com>
2024-04-28 06:07:15 +0600

    footer added

--- src/Layoutes/Root.jsx
+++ src/Layoutes/Root.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import Footer from "../pages/Footer";
 
 const Root = () => {
     return (
         <div >
             <Navbar/>
             <Outlet/>
+            <Footer/>
         </div>
     );
 };

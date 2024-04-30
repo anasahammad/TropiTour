8be51d059dc6a2bb47db06c0db7d110b2ec331e3
Anas Ahammad <anasahammad2002@gmail.com>
2024-04-30 14:01:07 +0600

    theme control problem solved

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,15 +6,19 @@ import Swal from "sweetalert2";
 
 
 const Navbar = () => {
-    const [theme, setTheme] = useState("theme")
+  const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light');
+
     const {user, signOutUser, loading} = useAuth()
     const [isOpen, setIsOpen] = useState(false)
+   
     
     const handleIsOpen = ()=>{
       setIsOpen(!isOpen)
     }
 
     const handleTheme = event =>{
+
+     
         if(event.target.checked){
             setTheme('dark')
             localStorage.setItem("theme", "dark")
@@ -23,6 +27,7 @@ const Navbar = () => {
             setTheme("light")
             localStorage.setItem("theme", "light")
         }
+        
     }
 
     useEffect(()=>{
@@ -61,7 +66,7 @@ const Navbar = () => {
           <label className="btn btn-circle swap swap-rotate">
       
     
-      <input type="checkbox" />
+      <input  type="checkbox" />
       
       <svg className="swap-off fill-current" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 512 512"><path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z"/></svg>
       
@@ -90,7 +95,7 @@ const Navbar = () => {
       </div>
       <div className="navbar-end ">
       <label className="cursor-pointer mr-2 md:mr-4 grid place-items-center">
-  <input type="checkbox" onChange={handleTheme}  className="toggle theme-controller bg-base-content row-start-1 col-start-1 col-span-2 "/>
+  <input   checked={theme === 'dark'} type="checkbox"  onChange={handleTheme}  className="toggle theme-controller bg-base-content row-start-1 col-start-1 col-span-2 "/>
   <svg className="col-start-1 row-start-1 stroke-base-100 fill-base-100" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="12" r="5"/><path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4"/></svg>
   <svg className="col-start-2 row-start-1 stroke-base-100 fill-base-100" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
 </label>

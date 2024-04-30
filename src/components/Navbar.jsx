6cd6059e034ae35191a3648eaf1eadbb1bd49967
Anas Ahammad <anasahammad2002@gmail.com>
2024-04-30 14:13:54 +0600

    some common issue solved

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -99,7 +99,8 @@ const Navbar = () => {
   <svg className="col-start-1 row-start-1 stroke-base-100 fill-base-100" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="12" r="5"/><path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4"/></svg>
   <svg className="col-start-2 row-start-1 stroke-base-100 fill-base-100" xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
 </label>
-      { user  ? <div className="dropdown dropdown-end relative">
+{loading ? <div><span className="loading loading-ring loading-lg"></span></div> :
+      (user)   ? <div className="dropdown dropdown-end relative">
       
      
       <a className="my-anchor-element">
@@ -136,7 +137,7 @@ const Navbar = () => {
           </ul>
         </div> :
         
-        <Link to="/login" className="btn  bg-[#FA7436] text-white" > Sign In</Link>  }
+        <Link to="/login" className="btn  bg-[#FA7436] text-white" > Sign In</Link>   }
     
        
       
